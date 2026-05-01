070ace38e165ba8ac62a62c769b73fbdb8017a1f
Anurag Kashyap <anuragkashyap6912@gmail.com>
2026-05-01 21:59:29 +0530

    Revision of CH1

---
+++ REVISION/ch1.py
@@ -0,0 +1,73 @@
+# Q1. Swap without third variable
+# Take two numbers as input and swap them
+
+a = input("enter a number 'a' :-  ")
+b = input("enter a number 'b' :- ")
+print("numbers are swapped!!")
+print (f"the number 'a' is {b}")
+print (f"the number 'b' is {a}")
+
+
+# Q2. Type checker
+# Take input from user and print its data type.
+#  (Hint: input() always gives string — so think.)
+
+x = input("Enter something: ")
+
+try:
+    x = int(x)
+    print("Integer")
+except:
+    try:
+        x = float(x)
+        print("Float")
+    except:
+        print("String")
+
+# Q2. List input & sum
+# Take 5 numbers from user, store in list, print sum.
+
+a  = int(input("enter a number - "))
+b  = int(input("enter another number - "))
+c  = int(input("enter another number - "))
+d  = int(input("enter another number - "))
+e  = int(input("enter another number - "))
+
+l = [ ]
+l.append(a)
+l.append(b)
+l.append(c)
+l.append(d)
+l.append(e)
+sum = (a+b+c+d+e)
+print(f"sum of {l} is {sum}")
+
+# Q3. Simple interest
+# Take P, R, T and calculate SI.
+
+p = int(input("enter the principal amount - "))
+r = int(input("enter the rate of interest - "))
+t = int(input("enter time period - "))
+SI = ((p*r*t)/100)
+print(f"the simple interest of your principal {p} at a rate of {r} in {t}years is {SI}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
