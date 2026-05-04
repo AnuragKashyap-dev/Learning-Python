83999d2e776838d01aeae66a9be22343b27d4035
Anurag Kashyap <anuragkashyap6912@gmail.com>
2026-05-04 22:17:11 +0530

    Revison of ch3

--- REVISION/CH3.py
+++ REVISION/CH3.py
@@ -1,59 +1,113 @@
-# LEVEL 1 (Basic control)
-# Q1.Take a string and print:
-# first character
-# last character
+# # LEVEL 1 (Basic control)
+# # Q1.Take a string and print:
+# # first character
+# # last character
 
-str = "python"
-print(str[0])
-print(str[-1])
-# Q2.Take a string and print:
-# first 4 characters
-# last 4 characters
+# str = "python"
+# print(str[0])
+# print(str[-1])
+# # Q2.Take a string and print:
+# # first 4 characters
+# # last 4 characters
 
-str = "zebronics"
-print(str[0:4])
-print(str[5:])
+# str = "zebronics"
+# print(str[0:4])
+# print(str[5:])
 
-# Q3.Given:
+# # Q3.Given:
+# # s = "python"
+# # Print:
+# # pyt
+# # hon
+
 # s = "python"
+# print(s[0:3])
+# print(s[3:])
+
+# # Q4.Reverse a string using slicing.
+
+# str = "anurag"
+# print(str[::-1])    #bcz '::-1' this says that print from last to first bcz the skip value is -1
+
+# # Q5.Print this exactly:
+# # Hello
+# # World
+# # (using escape sequence)
+
+# print("hello\nworld")
+
+# # Q6.Convert a string to:
+# # uppercase
+# # lowercase
+
+# str  = "hemoglobin"
+# print(str.capitalize())
+# print(str.lower())
+# print(str)
+
+# # Q7.Count total characters in a string.
+
+# str = "royal"
+# print(len(str))
+
+# #LEVEL 2 (Concept building)
+
+# #Q8.Take a string and print every second character.
+
+# str = "royalchallengersbangalore"
+# print(str[0::2])
+
+# Q9.Take a string and print characters from index 2 to 8 with step 2.
+
+str = "horsecart"
+print(str[2:8:2])
+
+
+# Q10.=Given:
+# s = "programming"
 # Print:
-# pyt
-# hon
+# rga
+# min
 
-s = "python"
-print(s[0:3])
-print(s[3:])
+s = "programming"
+print(s[1:6:2])
+print(s[7:10])
 
-# Q4.Reverse a string using slicing.
 
-str = "anurag"
-print(str[::-1])    #bcz '::-1' this says that print from last to first bcz the skip value is -1
 
-# Q5.Print this exactly:
-# Hello
-# World
+
+
+# Q11.Check if a string starts with "A" and ends with "z".
+stt = "Anurag"
+print(stt.startswith("A"))
+print(stt.endswith("z"))
+
+
+# Q12.Replace all spaces in a string with _.
+spaces = "   a   n   u   r  a   g  "
+print(spaces.replace(" ","_"))
+
+
+# Q13.Count how many times a character appears in a string.
+str = "r o y a l"
+print(str.count("r o y a l"))
+
+
+
+# Q14.Print:
+# He said "Python is easy"
 # (using escape sequence)
 
-print("hello\nworld")
-
-# Q6.Convert a string to:
-# uppercase
-# lowercase
-
-str  = "hemoglobin"
-print(str.capitalize())
-print(str.lower())
+str = "he said \'python is easy\'"
 print(str)
 
-# Q7.Count total characters in a string.
+# LEVEL 3 
 
-str = "royal"
-print(len(str))
+# Q15.Take a string and:
+# print even index characters
+# print odd index characters
 
-#LEVEL 2 (Concept building)
-
-#Q8.Take a string and print every second character.
-
-str = "royalchallengersbangalore"
-print(str[0::2])
+string = "geforcertx"
+print(string[::2])  #even
+print(string[::3])  #odd
 
