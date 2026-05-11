cec9a86192d784681d9caeb5db0f47078c288c01
Anurag Kashyap <anuragkashyap6912@gmail.com>
2026-05-11 23:37:19 +0530

    Revising CH5

--- REVISION/CH5.py
+++ REVISION/CH5.py
@@ -1,82 +1,81 @@
-# # Practice Set ( Dictionaries & Sets)
-# # LEVEL 1
+# Practice Set ( Dictionaries & Sets)
+# LEVEL 1
 
-# # Q1.Create a dictionary with:
-# # name
-# # age
-# # marks
-# # Print all values.
+# Q1.Create a dictionary with:
+# name
+# age
+# marks
+# Print all values.
 
-# dict = {"name":"Anurag",
-#         "age":"16",
-#         "marks":"90"}
-# print(dict.items())         
+dict = {"name":"Anurag",
+        "age":"16",
+        "marks":"90"}
+print(dict.items())         
 
-# # Q2. Access and print only the "marks" value.
+# Q2. Access and print only the "marks" value.
 
-# dict = {"name":"Anurag",
-#         "age":"16",
-#          "marks":"90"}
+dict = {"name":"Anurag",
+        "age":"16",
+         "marks":"90"}
 
-# print(dict.get("marks"))
+print(dict.get("marks"))
 
-# # Q3.Add a new key "city" to a dictionary.
+# Q3.Add a new key "city" to a dictionary.
 
-# dict = {"name":"Anurag",
-#         "age":"16",
-#          "marks":"90"}
-# dict["city"] = "delhi"
-# print(dict)
-
-
-# # Q4. Change value of an existing key.
-
-# dict = {"name":"Anurag",
-#         "age":"16",
-#          "marks":"90"}
-
-# dict.update({"name":"AVINASH"})
-# print(dict.get("name"))
-
-# # Q5. Print all keys of a dictionary.
-
-# dict = {"name":"Anurag",
-#         "age":"16",
-#          "marks":"90"}
-
-# print(dict.keys())
-
-# # Q6.Print all values of a dictionary.
-
-# dict = {"name":"Anurag",
-#          "age":"16",
-#          "marks":"90"}
-# print(dict.values())
-
-# # Q7.Create a set with duplicate values and observe output.
-# s = {1,1,2,3,3,3,2,2,3,4,3,4,3,3,4,3,4,3}
-# print(s)
-
-# '''output: - {1,2,3,4}
-# --> bcz sets can't contain duplicate values'''
+dict = {"name":"Anurag",
+        "age":"16",
+         "marks":"90"}
+dict["city"] = "delhi"
+print(dict)
 
 
-# # Q8.Add an element to a set.
-# s = {1,2,34,4,6,6}
-# s.add("anurag")
-# print(s)
+# Q4. Change value of an existing key.
 
-# # Q9.Remove an element from a set.
-# s = {1,2,3,4,5,6,7,8,}
-# s.remove(1)
-# print(s)
+dict = {"name":"Anurag",
+        "age":"16",
+         "marks":"90"}
+
+dict.update({"name":"AVINASH"})
+print(dict.get("name"))
+
+# Q5. Print all keys of a dictionary.
+
+dict = {"name":"Anurag",
+        "age":"16",
+         "marks":"90"}
+
+print(dict.keys())
+
+# Q6.Print all values of a dictionary.
+
+dict = {"name":"Anurag",
+         "age":"16",
+         "marks":"90"}
+print(dict.values())
+
+# Q7.Create a set with duplicate values and observe output.
+s = {1,1,2,3,3,3,2,2,3,4,3,4,3,3,4,3,4,3}
+print(s)
+
+'''output: - {1,2,3,4}  bcz sets can't contain duplicate values'''
+
+
+# Q8.Add an element to a set.
+s = {1,2,34,4,6,6}
+s.add("anurag")
+print(s)
+
+# Q9.Remove an element from a set.
+s = {1,2,3,4,5,6,7,8,}
+s.remove(1)
+print(s)
 
 
 # LEVEL 2
 
 # Q10.Count frequency of elements using dictionary.
-# dict = {1,1,1,1,2,2,2,2,4,4,4}
-# print()
+
+
 
 
 
@@ -91,28 +90,35 @@ if "marks" in dict:
 else:
         print("new one")
 
-# Q12.Merge two dictionaries.
-dict1 = {"anurag"}
-dict2 = {"kashyap"}
-print(dict1.add(dict2))
+# # Q12.Merge two dictionaries.
+# dict1 = {"anurag":"kashyap"}
+# dict2 = {"kashyap":"anurag"}
 
-# Q13
 
-# Create a dictionary from two lists:
-
+# Q13.Create a dictionary from two lists:
 # keys = ["name", "age"]
 # values = ["Anurag", 15]
-# Q14
+# dict = {}
+# dict.update(keys = ["name", "age"],
+# values = ["Anurag", 15])
+# print(dict)
 
-# Find common elements between two sets.
+# Q14.Find common elements between two sets.
+s = {1,2,3}
+s2 = {3,4,5}
+print(s.intersection(s2))
 
-# Q15
+# # Q15.Find union of two sets.
+s = {1,2,3}
+s2 = {3,4,5}
+print(s.union(s2))
 
-# Find union of two sets.
 
-# Q16
+# # Q16.Find difference between two sets.
+s = {1,2,3,4}
+s2 = {2,5,6}
+print(s.difference(s2))
 
-# Find difference between two sets.
 
 # LEVEL 3
 # Q17
