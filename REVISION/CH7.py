7f1a1001ecdb9b68b5f9de73914599d886b13dab
Anurag Kashyap <anuragkashyap6912@gmail.com>
2026-05-20 17:47:05 +0530

    Questions for Ch7 Revision

---
+++ REVISION/CH7.py
@@ -0,0 +1,47 @@
+1. Print numbers from 1 to 10 using both for and while loop.
+
+2. Print all even numbers between 1 and 50.
+
+3. Find sum of first n natural numbers.
+
+4. Print multiplication table of a number.
+
+5. Find factorial of a number.
+
+6. Reverse a number.
+
+7. Check whether a number is palindrome.
+
+8. Check whether a number is prime.
+
+9. Print Fibonacci series up to n terms.
+
+10. Find sum of digits of a number.
+
+11. Count vowels in a string using loop.
+
+12. Reverse a string using loop.
+
+13. Find largest element in a list using loop.
+
+14. Remove duplicates from a list manually.
+
+15. Print pattern:
+*
+**
+***
+****
+
+16. Print pattern:
+****
+***
+**
+*
+
+17. Create login system with 3 attempts.
+
+18. Create menu-driven calculator using loop.
+
+19. Keep taking input until user enters 0.
+
+20. Guess the number game using loop.
