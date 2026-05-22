370d1ba3520c52ec14e9646d140b229e125a7924
Anurag Kashyap <anuragkashyap6912@gmail.com>
2026-05-22 19:29:51 +0530

    Revision CH7

--- REVISION/CH7.py
+++ REVISION/CH7.py
@@ -16,6 +16,14 @@ for i in range(1,51):
 
 3. Find sum of first n natural numbers.
 
+n = int(input("Enter a number: "))
+sum = 0
+
+for i in range(1,n + 1):
+    sum = sum + i
+
+print("Sum =", sum)
+
 4. Print multiplication table of a number.
 
 5. Find factorial of a number.
