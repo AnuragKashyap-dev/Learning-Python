30755671f58d932f1f336a56729700d5e208f6f4
Anurag Kashyap <anuragkashyap6912@gmail.com>
2026-05-28 23:32:28 +0530

    Revision CH7

--- REVISION/CH7.py
+++ REVISION/CH7.py
@@ -1,5 +1,5 @@
-1. Print numbers from 1 to 10 using both for and while loop.
-
+# 1. Print numbers from 1 to 10 using both for and while loop.
+'''
 for i in range(1,11):
   print(i)
 
@@ -8,13 +8,13 @@ while i <= 10:
   print(i)
   i+=1
 
-2. Print all even numbers between 1 and 50.
+# 2. Print all even numbers between 1 and 50.
 
 for i in range(1,51):
   if i%2==0:
     print(i)
 
-3. Find sum of first n natural numbers.
+# 3. Find sum of first n natural numbers.
 
 n = int(input("Enter a number: "))
 sum = 0
@@ -22,20 +22,21 @@ sum = 0
 for i in range(1,n + 1):
     sum = sum + i
 
-print("Sum =", sum)
+print(f"Sum = {sum}")
 
-4. Print multiplication table of a number.
+# 4. Print multiplication table of a number.
 
 num = 8
 for i in range(1,11):
   print(f"{num} X {i} = {i*num}")
 
+"or"
 
 num = int(input("enter a number:- "))
 for i in range(1,11):
   print(f"{num} X {i} = {i*num}")
 
-5. Find factorial of a number.
+# 5. Find factorial of a number.
 
 num = int(input("Enter a number: "))
 
@@ -46,46 +47,71 @@ for i in range(1, num + 1):
 
 print(f"Factorial = {int}")
 
-6. Reverse a number.
+# 6. Reverse a number.
 
-7. Check whether a number is palindrome.
+# 7. Check whether a number is palindrome.
 
-8. Check whether a number is prime.
+# 8. Check whether a number is prime.
 
-9. Print Fibonacci series up to n terms.
+# 9. Print Fibonacci series up to n terms.
 
-10. Find sum of digits of a number.
+# 10. Find sum of digits of a number.
 
-11. Count vowels in a string using loop.
+# 11. Count vowels in a string using loop.
 
-12. Reverse a string using loop.
+# 12. Reverse a string using loop.
 
-13. Find largest element in a list using loop.
+# 13. Find largest element in a list using loop.
 
-14. Remove duplicates from a list manually.
+# 14. Remove duplicates from a list manually.
 
-15. Print pattern:
-*
-**
-***
-****
+# 15. Print pattern:
+# *
+# **
+# ***
+# ****
 
 for i in range(1,5):
   print("*"*i)
 
-16. Print pattern:
-****
-***
-**
-*
+# 16. Print pattern:
+# ****
+# ***
+# **
+# *
 
 for i in range(4,0,-1):
   print("*"*i)
 
-17. Create login system with 3 attempts.
+# 17. Create login system with 3 attempts.
 
-18. Create menu-driven calculator using loop.
+print("---- LOGIN ----")
+user = "anurag"
+password = 122345
+max_attempts = 3
+attempts = 0
 
-19. Keep taking input until user enters 0.
+while(attempts<max_attempts):
+    username = input("Enter Username:- ")
+    psw = int(input("Enter Password:- "))
 
-20. Guess the number game using loop.
+    if username == user and psw == password:
+        print("Login Successfull")
+    else:
+        attempts +=1
+        print("Invalid Pair !!!")
+if attempts == max_attempts:
+    print("too many attempts , access denied")
+
+'''
+# 18. Create menu-driven calculator using loop.
+
+# 19. Keep taking input until user enters 0.
+number = 0
+num = int(input("Enter Number:- "))
+while(num != number):
+    print("try again")
+    num = int(input("Enter Number:- "))
+print("succesfull")
+
+# 20. Guess the number game using loop.
