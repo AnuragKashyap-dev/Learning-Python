16d7aca2efa9b7b7c101e87f8c2e631f957a1e6e
Anurag Kashyap <anuragkashyap6912@gmail.com>
2026-06-02 23:48:20 +0530

    clearing basics (loops)

---
+++ REVISION/basics_of_loops.py
@@ -0,0 +1,18 @@
+# Sum of all numbers in a list
+l = [1,2,3,5]
+total = 0
+for num in l:
+    total = num+total
+print(total)    
+# Count odd numbers in a list
+l = [1,2,3,5]
+odd = 0 
+for num in l :
+    if num%2 != 0 :
+        odd +=1
+print(odd)        
+# Find smallest number in a list
+
+# Sum of even numbers only
+
+# Find largest odd number in a list
