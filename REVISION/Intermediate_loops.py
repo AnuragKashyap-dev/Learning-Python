e56c835a8b45cfa65f6fce55fb11655c9203bd20
Anurag Kashyap <anuragkashyap6912@gmail.com>
2026-07-13 22:25:37 +0530

    Update Intermediate_loops.py

--- REVISION/Intermediate_loops.py
+++ REVISION/Intermediate_loops.py
@@ -504,6 +504,9 @@ for nums in l :
 print(second)
       
 # Q47.Find the second smallest distinct element.
+
 # Q48.Find the most frequent element in a list.
+
 # Q49.Find the least frequent element in a list.
+
 # Q50.Find all elements that occur exactly once.
