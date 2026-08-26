0af0af35f126ec94545ebda76f309bd9b2ae4a83
Anurag Kashyap <anuragkashyap6912@gmail.com>
2026-08-26 21:16:51 +0530

    Update number guess game.py

--- Projects/number guess game.py
+++ Projects/number guess game.py
@@ -1,8 +1,10 @@
-awhile num != a:
-    a = int(input("enter a number:- "))
-    if num<a:
-        print("smaller")
-    elif num>a:
-        print("larger")
-    elif num == a:
-        print("you caught that!!")
+num = 9
+a = int(input("enter a number:- "))
+while num != a:
+  if num<a:
+      print("smaller")
+  elif num>a:
+      print("larger")
+  elif num == a:
+      print("you caught that!!")
+  a = int(input("enter a number:- "))
